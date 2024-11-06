ece68953561db7f06151d6736fdcde4ca4b4e1ec
JetGreen <grishadzyin@gmail.com>
2024-11-06 21:38:46 +0500

    upload is working

--- src/solution/solution.controller.ts
+++ src/solution/solution.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query  } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, UploadedFile, HttpCode, Delete, Query, HttpStatus, UseInterceptors, UploadedFiles } from '@nestjs/common';
 import { SolutionService } from './solution.service';
+
+import { FilesInterceptor } from '@nestjs/platform-express';
 
 // all about MongoDB
 import { InjectModel } from '@nestjs/mongoose';
@@ -13,10 +15,52 @@ export class SolutionController {
     @InjectModel('Solution') private SolutionModel: Model<SolutionClass>,
     private readonly solutionService: SolutionService) { }
 
-    @Post('')
-    async newSolution(
-      @Body('solution') solution: any
-    ) {
-      return await this.SolutionModel.create(solution)
-    }
+  @Post('')
+  async newSolution(
+    @Body('solution') solution: any
+  ) {
+    return await this.SolutionModel.create(solution)
+  }
+
+  @Post('upload/folder')
+  @UseInterceptors(FilesInterceptor('files', 100)) // 'files' is the form field name, max 100 files
+  uploadFolder(@UploadedFiles() files: Express.Multer.File[]) {
+    const fileNames = files.map(file => file.filename);
+    // console.log(files);
+    
+    return {
+      message: 'Files uploaded successfully',
+      files: fileNames,
+    };
+  }
+
+  @Post('upload/archives')
+  @UseInterceptors(FilesInterceptor('files', 10)) // 'files' is the form field name, max 10 files
+  uploadArchives(@UploadedFiles() files: Express.Multer.File[]) {
+    const fileNames = files.map(file => file.filename);
+    return {
+      message: 'Files uploaded successfully',
+      files: fileNames,
+    };
+  }
+
+  @Post('upload/any-files')
+  @UseInterceptors(FilesInterceptor('files', 40)) // 'files' is the form field name, max 40 files
+  uploadAnyFiles(@UploadedFiles() files: Express.Multer.File[]) {
+    const fileNames = files.map(file => file.filename);
+    return {
+      message: 'Files uploaded successfully',
+      files: fileNames,
+    };
+  }
+
+  @Post('upload/code')
+  @UseInterceptors(FilesInterceptor('files', 40)) // 'code' is the form field name, max 40 files
+  uploadCode(@UploadedFiles() files: Express.Multer.File[]) {    
+    const fileNames = files.map(file => file.filename);
+    return {
+      message: 'Files uploaded successfully',
+      files: fileNames,
+    };
+  }
 }
--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -7,10 +7,10 @@ export class VideoService {
   // Your business logic around video processing can go here
   async handleFileUpload(file: Express.Multer.File) {
     let customFilename = Date.now() + '_' + file.originalname;
-    const filePath = join(__dirname, '..', '..', 'public', 'lesson-videos', customFilename);
+    const filePath = join(__dirname, '..', '..', 'public', 'solution-folders', customFilename);
     await writeFile(filePath, file.buffer);
     return {
-      message: 'Video uploaded successfully!',
+      message: 'Folder uploaded successfully!',
       filename: customFilename
     };
   }
