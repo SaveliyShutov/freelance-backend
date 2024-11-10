a28f72d03ab123250b8726b6403935c70363dddb
JetGreen <grishadzyin@gmail.com>
2024-11-10 22:07:34 +0500

    a

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({ 
-    origin: [process.env.CLIENT_URL, 'http://localhost:3011'],
+    origin: [process.env.CLIENT_URL, 'http://edu.factum-studio.com/', 'https://edu.factum-studio.com/'],
     credentials: true
   })
   app.useGlobalFilters(new HttpExceptionFilter())
