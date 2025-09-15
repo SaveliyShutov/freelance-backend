0d6d3d7d50389d261ebfd3ea19c15c13004441a7
MrMefedron <gleb70706@gmail.com>
2025-09-15 12:47:08 +0500

    changing telegram1

--- src/order/order.telegram.service.ts
+++ src/order/order.telegram.service.ts
@@ -1,4 +1,10 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Telegraf } from 'telegraf';
+
+import { OrderClass } from './schemas/order.schema';
+import { UserClass } from '../user/schemas/user.schema';
 
 @Injectable()
 export class TelegramService {
